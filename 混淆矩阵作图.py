1f1c61a68144afc0025af67f866a9f2802cde1da
YANGXIAORUN <33563685+yangxiaorun@users.noreply.github.com>
2019-09-04 17:12:54 +0800

    Update 混淆矩阵作图.py

--- 混淆矩阵作图.py
+++ 混淆矩阵作图.py
@@ -57,4 +57,5 @@ if __name__ == '__main__':
     现在的趋势是尽量避免全连接，目前主流的一个方法是全局平均值。
     也就是最后那一层的feature map（最后一层卷积的输出结果），直接求平均值。有多少种分类就训练多少层，这十个数字就是对应的概率或者叫置信度。
     https://blog.csdn.net/whyymlm/article/details/79739840 CNN（卷积层convolutional layer，激励层activating layer，池化层pooling，全连接层fully connected）
+    https://blog.csdn.net/boon_228/article/details/81238091 理解卷积神经网络CNN中的特征图 feature map
     '''
